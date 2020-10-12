e15e1be63878507af2dbc7c6614185c2648d0694
候泽空 <7834952+hou_zekong@user.noreply.gitee.com>
2020-10-12 10:00:00 +0800

    [候泽空]添加了首页余额折线图

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">欢迎您: {{ name }}</div>
+    <div class="dashboard-signupCharts" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 
 export default {
   name: 'Dashboard',
   computed: {
     ...mapGetters([
       'name'
-    ])
+    ]),
+    ...mapState('signupcheck', ['signupcheck'])
+  },
+  mounted() {
+    console.log(this.signupcheck)
+    const myChart = this.$echarts.init(document.getElementsByClassName('dashboard-signupCharts')[0])
+    myChart.setOption({
+      title: {
+        text: '报名信息总览'
+      },
+      tooltip: {},
+      xAxis: {
+        data: this.signupcheck.map((item) => {
+          return item.realname
+        })
+      },
+      yAxis: {},
+      series: [{
+        name: '账户余额',
+        type: 'line',
+        data: this.signupcheck.map((item) => {
+          return item.money
+        })
+      }]
+    })
   }
 }
 </script>
@@ -26,5 +51,10 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+}
+.dashboard-signupCharts{
+  width: 85%;
+  height: 280px;
+  margin-top: 20px;
 }
 </style>
