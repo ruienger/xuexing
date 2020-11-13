6a72be66666e6145a2d3936431eca6ae4ede6f8e
候泽空 <7834952+hou_zekong@user.noreply.gitee.com>
2020-11-13 17:30:05 +0800

    [候泽空]更新图表,有些小细节、小bug待修复

--- src/views/dashboard/admin/components/BarChart.vue
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -38,6 +38,70 @@ export default {
       chart: null
     }
   },
+  watch:{
+    chartData:{
+      deep: true,
+      handler(){
+        this.chart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          top: 10,
+          left: '2%',
+          right: '2%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          data: this.Xopts,
+          axisTick: {
+            alignWithLabel: true
+          }
+        }],
+        yAxis: [{
+          type: 'value',
+          axisTick: {
+            show: false
+          }
+        }],
+        series: [{
+          name: '报名中',
+          type: 'bar',
+          stack: 'vistors',
+          barWidth: '60%',
+          data: this.chartData['报名中'],
+          animationDuration
+        }, {
+          name: '审核中',
+          type: 'bar',
+          stack: 'vistors',
+          barWidth: '60%',
+          data: this.chartData['审核中'],
+          animationDuration
+        }, {
+          name: '游学中',
+          type: 'bar',
+          stack: 'vistors',
+          barWidth: '60%',
+          data: this.chartData['游学中'],
+          animationDuration
+        }, {
+          name: '已完成',
+          type: 'bar',
+          stack: 'vistors',
+          barWidth: '60%',
+          data: this.chartData['已完成'],
+          animationDuration
+        }]
+      })
+      }
+    }
+  },
   mounted() {
     this.$nextTick(() => {
       this.initChart()
